193bc58ecad6555d4c8132b0e439eebba9584912
Lab_computer <gvongastrow@eng.ucsd.edu>
2019-04-09 18:24:19 -0700

    debug

--- setHotPlateTemperature.m
+++ setHotPlateTemperature.m
@@ -4,6 +4,7 @@ function [success] = setHotPlateTemperature(app,hotplateNumber,temperature)
 %   hotplateNumber: a number corresponding to the hotplate
 %   temperature: the temperature in °C
     % Get the handle to the hotplate
+    Tnew = CorrectT_ext(temperature,hotplateNumber);
     hotplate_handle = app.HW(hotplateNumber).HP;
     HT = floor(temperature/25.6); %Set high temp value 
     LT = mod(temperature,25.6)*10; %Set low temp value 
@@ -23,8 +24,8 @@ function [success] = setHotPlateTemperature(app,hotplateNumber,temperature)
             success = 0;
         else
             success = 1;
-            fprintf("Temperature set to %.1f °C on hotplate %d.",...
-                temperature,hotplateNumber);
+            fprintf("Temperature set to %.1f °C on hotplate %d, corresponding to a surface temperature of %.1f",...
+                temperature,hotplateNumber,Tnew);
         end
     catch
         warndlg(sprintf('Error communicating with hotplate %d',hotplateNumber),'Hotplate error');
