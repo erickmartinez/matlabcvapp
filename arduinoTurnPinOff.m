3c4cfb65251f270a2999237248435e7ee3a4ac4f
erickmartinez <erickrmartinez@gmail.com>
2019-04-09 20:54:36 -0700

    Clear the handles to the arduino on the toggle pin functions
    Clear the handles to the arduino on the toggle pin functions

--- arduinoTurnPinOff.m
+++ arduinoTurnPinOff.m
@@ -1,4 +1,12 @@
-function arduinoTurnPinOff(app,arduino_handle,pinNumber)
-% Turn the specified pin off on the arduino
-    arduinoTogglePin(app,arduino_handle, pinNumber,0);
+function arduinoTurnPinOff(app,arduinoNumber,pinNumber)
+% Turn the specified pin on on the arduino
+% Parameters
+% ----------
+% app : obj
+%   A handle to the app designer gui object
+% arduinoNumber : int
+%   The number of the arduino to communicate with
+% pinNumber : int
+%   The number of the pin to be turned off
+    arduinoTogglePin(app,arduinoNumber, pinNumber,0);
 end
